167ed18b368d82caa3528eb3651ab79700b4026f
Burak Çakır <burak2043@gmail.com>
2022-09-01 13:09:19 +0300

    emoji butonu eklendi

--- src/components/ChatContent/index.jsx
+++ src/components/ChatContent/index.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { IoCallOutline } from 'react-icons/io5'
 import { BsCameraVideo } from 'react-icons/bs'
 import { FiMoreHorizontal } from 'react-icons/fi'
+import Picker from 'emoji-picker-react';
 
 const ChatContent = () => {
 
     const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+    const [emojiIsVisible, setEmojiIsVisible] = useState(false);
 
     return (
         <div id='chat-content' className='w-100'>
@@ -42,8 +45,13 @@ const ChatContent = () => {
             <div className="chat-content-messages flex-fill">
                 yazilan mesajların listelendiği alan
             </div>
-            <div className="chat-content-input">
-                yeni mesaj yazma alanı
+            <div className="chat-content-input position-relative">
+                <div className='emoji-pick' style={emojiIsVisible ? { position: "absolute", bottom: 65 } : { display: "none", visibility: "hidden" }}>
+                    <Picker />
+                </div>
+                <button onClick={() => setEmojiIsVisible(!emojiIsVisible)} style={{ border: "1px solid gray" }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-smile"><circle cx="12" cy="12" r="10"></circle><path d="M8 14s1.5 2 4 2 4-2 4-2"></path><line x1="9" y1="9" x2="9.01" y2="9"></line><line x1="15" y1="9" x2="15.01" y2="9"></line></svg>
+                </button>
             </div>
         </div>
     )
