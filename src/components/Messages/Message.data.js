4cc12429308b21cd3fdcc874fb75dce79ada223c
Burak Çakır <burak2043@gmail.com>
2022-09-07 14:24:03 +0300

    mesajların listelenmesi tamamlandi ve küçük hatalar giderildi

--- src/components/Messages/Message.data.js
+++ src/components/Messages/Message.data.js
@@ -13,5 +13,67 @@ export const messages = [
         me: true,
         message: "Tanıştığımıza memnun oldum",
         time: "20:44 AM"
+    },
+    {
+        me: false,
+        message: "Denizli",
+        time: "20:45 AM"
+    }, {
+        me: false,
+        message: "Merhaba",
+        time: "20:43 AM"
+    },
+    {
+        me: true,
+        message: "Naber",
+        time: "20:44 AM"
+    },
+    {
+        me: true,
+        message: "Tanıştığımıza memnun oldum",
+        time: "20:44 AM"
+    },
+    {
+        me: false,
+        message: "Denizli",
+        time: "20:45 AM"
+    }, {
+        me: false,
+        message: "Merhaba",
+        time: "20:43 AM"
+    },
+    {
+        me: true,
+        message: "Naber",
+        time: "20:44 AM"
+    },
+    {
+        me: true,
+        message: "Tanıştığımıza memnun oldum",
+        time: "20:44 AM"
+    },
+    {
+        me: false,
+        message: "Denizli",
+        time: "20:45 AM"
+    }, {
+        me: false,
+        message: "Merhaba",
+        time: "20:43 AM"
+    },
+    {
+        me: true,
+        message: "Naber",
+        time: "20:44 AM"
+    },
+    {
+        me: true,
+        message: "Tanıştığımıza memnun oldum",
+        time: "20:44 AM"
+    },
+    {
+        me: false,
+        message: "Denizli",
+        time: "20:45 AM"
     }
 ] 
--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -3,11 +3,9 @@ import styles from './Message.module.css'
 const Messages = ({ message }) => {
 
     return (
-        <div style={message.me ? { float: "right" } : {}}>
-            <div className='d-flex w-100 align-items-end'>
-                <p className={styles.other} style={message.me ? { backgroundColor: "#0a80ff", color: "#fff" } : {}}>{message.message}</p>
-                <p className={styles.time}>{message.time}</p>
-            </div>
+        <div className={`d-flex w-100 align-items-end ${message.me ? 'justify-content-end' : ''}`}>
+            <p className={styles.other} style={message.me ? { backgroundColor: "#0a80ff", color: "#fff" } : {}}>{message.message}</p>
+            <p className={styles.time}>{message.time}</p>
         </div>
     )
 }
